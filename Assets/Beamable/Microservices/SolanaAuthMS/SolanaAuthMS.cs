b77b18cd1bd4662416c0f70b582c519b20bd205e
Ali El Rhermoul <aelrhermoul@gmail.com>
2023-01-10 15:20:40 -0500

    Fixed dependencies for Solana Microservice

--- Assets/Beamable/Microservices/SolanaAuthMS/SolanaAuthMS.cs
+++ Assets/Beamable/Microservices/SolanaAuthMS/SolanaAuthMS.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Text;
 using Beamable.Server;
+using Beamable.Common.Api.Auth;
 using Solana.Unity.Wallet;
 using UnityEngine;
 
@@ -9,28 +10,28 @@ namespace Beamable.Microservices
 	[Microservice("SolanaAuthMS")]
 	public class SolanaAuthMS : Microservice
 	{
-		// [ClientCallable("Solana/authenticate")]
-		// public ExternalAuthenticationResponse Authenticate(string token, string challenge, string solution)
-		// {
-		// 	Debug.Log($"Token: {token}, challenge: {challenge}, solution: {solution}");
-		//
-		// 	if (!IsValid(challenge, solution))
-		// 	{
-		// 		Debug.Log("Challenge or solution is empty");
-		// 		return new ExternalAuthenticationResponse {challenge = Guid.NewGuid().ToString(), challenge_ttl = 60};
-		// 	}
-		//
-		// 	if (Verify(token, challenge, solution))
-		// 	{
-		// 		Debug.Log("Verified");
-		// 		return new ExternalAuthenticationResponse {user_id = token};
-		// 	}
-		//
-		// 	Debug.Log("Not verified");
-		// 	return new ExternalAuthenticationResponse {challenge = Guid.NewGuid().ToString(), challenge_ttl = 60};
-		// }
+         [ClientCallable("Solana/authenticate")]
+         public ExternalAuthenticationResponse Authenticate(string token, string challenge, string solution)
+         {
+         	Debug.Log($"Token: {token}, challenge: {challenge}, solution: {solution}");
+        
+         	if (!IsValid(challenge, solution))
+         	{
+         		Debug.Log("Challenge or solution is empty");
+         		return new ExternalAuthenticationResponse {challenge = Guid.NewGuid().ToString(), challenge_ttl = 60};
+         	}
+        
+         	if (Verify(token, challenge, solution))
+         	{
+         		Debug.Log("Verified");
+         		return new ExternalAuthenticationResponse { user_id = token};
+         	}
+        
+         	Debug.Log("Not verified");
+         	return new ExternalAuthenticationResponse {challenge = Guid.NewGuid().ToString(), challenge_ttl = 60};
+         }
 
-		private bool IsValid(string challenge, string solution)
+        private bool IsValid(string challenge, string solution)
 		{
 			return !string.IsNullOrEmpty(challenge) && !string.IsNullOrEmpty(solution);
 		}
