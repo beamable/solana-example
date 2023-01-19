c3b29fb7f13712846210cc6e89086d3ddcadaf9b
Drazen Patekar <dpatekar@gmail.com>
2023-01-19 10:16:55 +0100

    Remove Solnet and Solana.Unity-SDK to make things work with Core

--- Assets/Plugins/Solana.Unity.SDK/codebase/nft/nft_interfaces/iNftFile.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Solana.Unity.SDK.Nft { 
-    public interface iNftFile<T> {
-        string name { get; set; }
-        string extension { get; set; }
-        string externalUrl { get; set; }
-        T file { get; set; }
-    }
-}
