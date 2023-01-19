e78971c24c3845340fcdaa47e9bc90f5fa60efef
Drazen Patekar <dpatekar@gmail.com>
2023-01-19 13:47:52 +0100

    Cosmetics

--- Assets/Beamable/Microservices/SolanaFederation/Features/Wallets/WalletService.cs
+++ Assets/Beamable/Microservices/SolanaFederation/Features/Wallets/WalletService.cs
@@ -30,7 +30,7 @@ namespace Beamable.Microservices.SolanaFederation.Features.Wallets
 			var insertSuccessful = await ValutCollection.TryInsert(db, newPersistedWallet);
 			if (insertSuccessful)
 			{
-				BeamableLogger.Log("Created realm wallet '{RealmWalletName}' {RealmWallet}", Configuration.RealmWalletName,
+				BeamableLogger.Log("Created realm wallet {RealmWalletName} {RealmWallet}", Configuration.RealmWalletName,
 					newWallet.Account.PublicKey.Key);
 				if (Configuration.AirDropAmount > 0) await newWallet.Account.Airdrop(Configuration.AirDropAmount);
 				return newWallet;
