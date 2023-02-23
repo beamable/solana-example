b566aac2513be1c844a23ce3ad1faad8c53adb2f
Łukasz Kundys <lukaszkundys@gmail.com>
2023-02-23 10:16:15 +0100

    Added proper namespaces

--- Packages/com.beamable.solana/Runtime/SolanaConfiguration/Attributes/HideIfAttribute.cs
+++ Packages/com.beamable.solana/Runtime/SolanaConfiguration/Attributes/HideIfAttribute.cs
@@ -1,74 +1,83 @@
 using System;
 using UnityEngine;
-using System.Collections;
 
-public class HidingAttribute : PropertyAttribute { }
-
-/// <summary>
-/// Hides a field if the bool 'variable' has the state 'state'
-/// </summary>
-[AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
-public class HideIfAttribute : HidingAttribute {
-
-    public readonly string variable;
-    public readonly bool state;
-
-    public HideIfAttribute(string variable, bool state, int order = 0) {
-        this.variable = variable;
-        this.state = state;
-        this.order = order;
+namespace Beamable.Solana.Configuration
+{
+    public class HidingAttribute : PropertyAttribute
+    {
     }
-}
 
-/// <summary>
-/// Hides a field if the Object 'variable' is null
-/// </summary>
-[AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
-public class HideIfNullAttribute : HidingAttribute {
+    /// <summary>
+    /// Hides a field if the bool 'variable' has the state 'state'
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
+    public class HideIfAttribute : HidingAttribute
+    {
+        public readonly string variable;
+        public readonly bool state;
 
-    public readonly string variable;
-
-    public HideIfNullAttribute(string variable, int order = 0) {
-        this.variable = variable;
-        this.order = order;
+        public HideIfAttribute(string variable, bool state, int order = 0)
+        {
+            this.variable = variable;
+            this.state = state;
+            this.order = order;
+        }
     }
-}
 
-/// <summary>
-/// Hides a field if the Object 'variable' is not null
-/// </summary>
-[AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
-public class HideIfNotNullAttribute : HidingAttribute {
+    /// <summary>
+    /// Hides a field if the Object 'variable' is null
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
+    public class HideIfNullAttribute : HidingAttribute
+    {
+        public readonly string variable;
 
-    public readonly string variable;
-
-    public HideIfNotNullAttribute(string variable, int order = 0) {
-        this.variable = variable;
-        this.order = order;
+        public HideIfNullAttribute(string variable, int order = 0)
+        {
+            this.variable = variable;
+            this.order = order;
+        }
     }
-}
 
-/// <summary>
-/// Hides a field based on it's enum value.
-/// use hideIf to specify if the variable must be equal to one of the attributes, or if it must be 
-/// unequal to all of the attributes
-/// </summary>
-[AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
-public class HideIfEnumValueAttribute : HidingAttribute {
+    /// <summary>
+    /// Hides a field if the Object 'variable' is not null
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
+    public class HideIfNotNullAttribute : HidingAttribute
+    {
+        public readonly string variable;
 
-    public readonly string variable;
-    public readonly int[] states;
-    public readonly bool hideIfEqual;
-
-    public HideIfEnumValueAttribute(string variable, HideIf hideIf, params int[] states) {
-        this.variable = variable;
-        this.hideIfEqual = hideIf == HideIf.Equal;
-        this.states = states;
-        this.order = -1;
+        public HideIfNotNullAttribute(string variable, int order = 0)
+        {
+            this.variable = variable;
+            this.order = order;
+        }
     }
-}
 
-public enum HideIf {
-    Equal,
-    NotEqual
+    /// <summary>
+    /// Hides a field based on it's enum value.
+    /// use hideIf to specify if the variable must be equal to one of the attributes, or if it must be 
+    /// unequal to all of the attributes
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
+    public class HideIfEnumValueAttribute : HidingAttribute
+    {
+        public readonly string variable;
+        public readonly int[] states;
+        public readonly bool hideIfEqual;
+
+        public HideIfEnumValueAttribute(string variable, HideIf hideIf, params int[] states)
+        {
+            this.variable = variable;
+            this.hideIfEqual = hideIf == HideIf.Equal;
+            this.states = states;
+            this.order = -1;
+        }
+    }
+
+    public enum HideIf
+    {
+        Equal,
+        NotEqual
+    }
 }
--- Packages/com.beamable.solana/Runtime/SolanaConfiguration/SolanaConfiguration.cs
+++ Packages/com.beamable.solana/Runtime/SolanaConfiguration/SolanaConfiguration.cs
@@ -2,56 +2,56 @@
 
 namespace Beamable.Solana.Configuration
 {
-	public class SolanaConfigurationConstants : IConfigurationConstants
-	{
-		public string GetSourcePath(Type type)
-		{
-			return "Packages/com.beamable.solana/Editor/SolanaConfiguration.asset";
-		}
-	}
+    public class SolanaConfigurationConstants : IConfigurationConstants
+    {
+        public string GetSourcePath(Type type)
+        {
+            return "Packages/com.beamable.solana/Editor/SolanaConfiguration.asset";
+        }
+    }
 
-	public class SolanaConfiguration : AbsModuleConfigurationObject<SolanaConfigurationConstants>
-	{
-		public enum RpcCluster
-		{
-			MainNet = 0,
-			DevNet = 1,
-			TestNet = 2,
-			Custom = 3
-		}
-		
-		public static SolanaConfiguration Instance => Get<SolanaConfiguration>();
+    public class SolanaConfiguration : AbsModuleConfigurationObject<SolanaConfigurationConstants>
+    {
+        public enum RpcCluster
+        {
+            MainNet = 0,
+            DevNet = 1,
+            TestNet = 2,
+            Custom = 3
+        }
 
-		public RpcCluster SelectedCluster;
+        public static SolanaConfiguration Instance => Get<SolanaConfiguration>();
 
-		[HideIfEnumValue("SelectedCluster", HideIf.NotEqual, (int)RpcCluster.Custom)]
-		public string CustomClusterAddress;
+        public RpcCluster SelectedCluster;
 
-		public int MaxRpcRequestsPerSec = 6;
-		public int AuthenticationChallengeTtlSec = 600;
-		public string RealmWalletName = "default-wallet";
-		public string DefaultTokenCollectionName = "Beamable";
-		public int AirDropAmount = 1;
-		
-		public string SolanaCluster => BuildClusterAddress();
-		public readonly string RealmSecret = Environment.GetEnvironmentVariable("SECRET");
+        [HideIfEnumValue("SelectedCluster", HideIf.NotEqual, (int)RpcCluster.Custom)]
+        public string CustomClusterAddress;
 
-		private string BuildClusterAddress()
-		{
-			return SelectedCluster == RpcCluster.Custom
-				? CustomClusterAddress
-				: $"https://api.{GetClusterString(SelectedCluster)}.solana.com";
-		}
+        public int MaxRpcRequestsPerSec = 6;
+        public int AuthenticationChallengeTtlSec = 600;
+        public string RealmWalletName = "default-wallet";
+        public string DefaultTokenCollectionName = "Beamable";
+        public int AirDropAmount = 1;
 
-		private static string GetClusterString(RpcCluster rpcCluster)
-		{
-			return rpcCluster switch
-			{
-				RpcCluster.MainNet => "mainnet-beta",
-				RpcCluster.DevNet => "devnet",
-				RpcCluster.TestNet => "testnet",
-				_ => "mainnet-beta"
-			};
-		}
-	}
+        public string SolanaCluster => BuildClusterAddress();
+        public readonly string RealmSecret = Environment.GetEnvironmentVariable("SECRET");
+
+        private string BuildClusterAddress()
+        {
+            return SelectedCluster == RpcCluster.Custom
+                ? CustomClusterAddress
+                : $"https://api.{GetClusterString(SelectedCluster)}.solana.com";
+        }
+
+        private static string GetClusterString(RpcCluster rpcCluster)
+        {
+            return rpcCluster switch
+            {
+                RpcCluster.MainNet => "mainnet-beta",
+                RpcCluster.DevNet => "devnet",
+                RpcCluster.TestNet => "testnet",
+                _ => "mainnet-beta"
+            };
+        }
+    }
 }
