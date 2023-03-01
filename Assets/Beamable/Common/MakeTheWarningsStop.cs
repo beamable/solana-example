991e9f28e72cc1136c2cb63f2fc3d1646095ed9c
Chris Hanna <chris@beamable.com>
2023-03-01 16:37:19 -0500

    client generation tweaks

---
+++ Assets/Beamable/Common/MakeTheWarningsStop.cs
@@ -0,0 +1,9 @@
+namespace Beamable.Common
+{
+	class MakeTheWarningsStop
+	{
+		// unless there is a script here, Unity will complain about not compiling the assembly. 
+		// this assembly is left here to demonstrate that under normal Beamable use cases, this directory exists for common code
+		// between Microservices and Unity.
+	}
+}
